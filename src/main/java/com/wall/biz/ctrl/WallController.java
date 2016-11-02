1cda1ead978d4303a659308f53cd2466be516574
wang.luo <wang.luo@msxf.com>
2016-11-02 17:56:24 +0800

    websockeserver增加

---
+++ src/main/java/com/wall/biz/ctrl/WallController.java
@@ -0,0 +1,8 @@
+package com.wall.biz.ctrl;
+
+/**
+ * @author: wang.luo
+ * @date 2016/11/2 17:55
+ */
+public class WallController {
+}
---
+++ src/main/java/com/wall/biz/service/WallService.java
@@ -0,0 +1,8 @@
+package com.wall.biz.service;
+
+/**
+ * @author: wang.luo
+ * @date 2016/11/2 17:55
+ */
+public class WallService {
+}
