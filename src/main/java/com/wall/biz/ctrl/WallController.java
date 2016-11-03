f5159ab7ba94057ddb60e9401e7914ad65199d6f
wangwang0925@gmail.com <8702Luo@@w999>
2016-11-04 00:54:45 +0800

    准备微信上墙消息逻辑

--- src/main/java/com/wall/biz/ctrl/WallController.java
+++ src/main/java/com/wall/biz/ctrl/WallController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
+ * 手机端发送上墙消息
  * @author: wang.luo
  * @date 2016/11/2 17:55
  */
@@ -16,6 +17,13 @@ public class WallController {
     @RequestMapping(value = "/sendMsgMobile/{activityId}")
     public ModelAndView sendMsgMobile(@PathVariable String activityId ){
         ModelAndView mv = new ModelAndView("wall/sendMsgMobile");
+        mv.addObject("activityId",activityId);
+        return mv;
+    }
+    @RequestMapping(value = "/show/{activityId}")
+    public ModelAndView show(@PathVariable String activityId ){
+        ModelAndView mv = new ModelAndView("wall/showMsg");
+        mv.addObject("activityId",activityId);
         return mv;
     }
 }
